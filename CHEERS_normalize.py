f79867ebe284111045c928480d1c65dbdf31e504
FaizRizvi <rizvifw@mail.uc.edu>
2021-09-15 13:23:05 -0400

    CHEERS_2.7 run modifications

--- CHEERS_normalize.py
+++ CHEERS_normalize.py
@@ -97,7 +97,7 @@ peakSums = []
 chrList = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22', 'chrX']
 
 # open and load all counts per files
-for filename in glob.glob(os.path.join(args.input, '*_ReadsInPeaks.txt')):
+for filename in glob.glob(os.path.join(args.input)): #, '*_ReadsInPeaks.txt')):
     file = open(filename, "r")
     fileText = file.read()
     file.close()
